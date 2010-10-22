d5e7d15585b6233ae9b94898809dbccb75f80c14
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2010-10-22 22:49:32 +0200

    asdad

--- Src/AssemblyInfo.fs
+++ Src/AssemblyInfo.fs
@@ -1,5 +1,16 @@
 ﻿namespace FSKit
 
+module Version =
+  let [<Literal>] Major = 0
+  let [<Literal>] Minor = 0
+  let [<Literal>] Build = 2
+  let [<Literal>] Revision = 0
+  let [<Literal>] Tag = "alpha"
+  let [<Literal>] String = "0.0.2.0"
+  let Tupled = (0, 0, 2, 0, Tag)
+  let Tagged = sprintf "%s-%s" String Tag
+  let FullName = sprintf "FSKit %s" String
+
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
